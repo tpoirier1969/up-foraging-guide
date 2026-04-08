03e8a05a2078bf269fbd90ecbb5288b32f34e361
Tod <tpoirier@nmu.edu>
2026-04-08 19:52:41 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.FORAGING_APP_CONFIG = {
-  supabaseUrl: 'PASTE_YOUR_SUPABASE_URL_HERE',
-  supabaseAnonKey: 'PASTE_YOUR_SUPABASE_ANON_KEY_HERE'
+  supabaseUrl: 'https://wntakzfoprthwggkidyq.supabase.co',
+  supabaseAnonKey: 'sb_publishable_gWu_EQ1J3s0iNjeDeINJwQ_xKy8QgAJ'
 };
