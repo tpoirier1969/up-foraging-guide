fc7e6f1cf8ebc034020786f391d082de46b2475e
Tod <tpoirier@nmu.edu>
2026-04-10 14:51:52 -0400

    Add wm3 constants file

---
+++ js/constants-wm3.js
@@ -0,0 +1,9 @@
+export const APP_VERSION = "v2.0-wm3";
+export const TABLE_NAME = "upper_michigan_foraging_species_v1";
+export const MONTHS = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+export const MONTH_SHORT = MONTHS.map(month => month.slice(0,3));
+export const PLANT_CATEGORIES = new Set(["Fruit","Green","Flower","Root","Tree Product","Green / Tubers"]);
+export const ACTIVE_NOW_LABEL = "In season";
+export const WEEKS = [1,2,3,4];
+
+export const VOCAB_VERSION = "v2.0-wm3";
