2eb75fca321df1d2b98a8c26627609bd419603a9
Tod <tpoirier@nmu.edu>
2026-04-12 17:31:03 -0400

    Use fixed detail renderer in masterlist

---
+++ js/app-mainfix4-masterlist-v8.js
@@ -0,0 +1,21 @@
+import { loadLocalData, loadSupabaseData, loadOverridePayload } from "./api-mainfix4.js?v=v2.1-mainfix23";
+import { loadLocalDataWithMasterV4 } from "./api-masterlist-v4.js";
+import { sortRecords, normalizeRecord } from "./data-model-mainfix4.js?v=v2.1-mainfix21";
+import { state } from "./state.js?v=v2.1-mainfix21";
+import { renderDashboard } from "./pages-mainfix4.js?v=v2.1-mainfix22";
+import { renderPage, bindDetailLinks, wireModal } from "./ui-mainfix-v2.js";
+
+async function init(){
+  wireModal();
+  let payload;
+  try{
+    payload = await loadSupabaseData();
+    payload = await loadLocalDataWithMasterV4(async()=>payload);
+  }catch{
+    payload = await loadLocalDataWithMasterV4(loadLocalData);
+  }
+  state.allRecords = sortRecords((payload.records||[]).map(normalizeRecord));
+  renderPage(renderDashboard({page:'home',allRecords:state.allRecords,currentRecords:state.allRecords}));
+  bindDetailLinks();
+}
+init();
