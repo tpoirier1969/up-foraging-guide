73cf7e1952e7f68f8b473fa9a3e69dc2fdb812b4
Tod <tpoirier@nmu.edu>
2026-04-21 22:30:20 -0400

    Refactor species loader sources to section-based paths

--- fixed-site/js/data/sources.js
+++ fixed-site/js/data/sources.js
@@ -1,8 +1,18 @@
-export const SPECIES_PATHS = [
-  "data/plants.json",
-  "data/plants-image-overrides.json",
-  "data/mushrooms.json"
-];
+export const SPECIES_SECTION_PATHS = {
+  plants: [
+    "data/plants.json",
+    "data/plants-image-overrides.json"
+  ],
+  mushrooms: [
+    "data/mushrooms.json"
+  ],
+  medicinal: [],
+  rare: []
+};
+
+export const SPECIES_PATHS = Object.entries(SPECIES_SECTION_PATHS).flatMap(([section, paths]) =>
+  paths.map(path => ({ section, path }))
+);
 
 export const OPTIONAL_PATHS = [
   "data/references-mainfix15.json",
