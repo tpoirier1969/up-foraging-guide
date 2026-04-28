2b8236234aff84899cf8f599057750a33bf5cde0
Tod <tpoirier@nmu.edu>
2026-04-28 15:20:06 -0400

    Add files via upload

--- fixed-site/js/data/load-app-data.js
+++ fixed-site/js/data/load-app-data.js
@@ -1,6 +1,6 @@
 import { fetchJsonFromRepo } from "../lib/fetch-json.js";
-import { mergeRecordLayers, normalizeRecord } from "../lib/merge.js?v=v4.2.61-r2026-04-28-mushroom-photo-batchpack1";
-import { SPECIES_PATHS, PHOTO_PATCH_PATHS, OPTIONAL_PATHS } from "./sources.js?v=v4.2.61-r2026-04-28-mushroom-photo-batchpack1";
+import { mergeRecordLayers, normalizeRecord } from "../lib/merge.js?v=v4.2.62-r2026-04-28-mushroom-photo-batchpack2";
+import { SPECIES_PATHS, PHOTO_PATCH_PATHS, OPTIONAL_PATHS } from "./sources.js?v=v4.2.62-r2026-04-28-mushroom-photo-batchpack2";
 
 let rareCachePromise = null;
 let referencesCachePromise = null;
