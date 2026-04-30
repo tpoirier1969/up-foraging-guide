ed94ccd5dfd3b3ed6086c46d3bdb24ce38f0eb8b
Tod <tpoirier@nmu.edu>
2026-04-30 14:50:42 -0400

    Add files via upload

--- fixed-site/js/data/sources.js
+++ fixed-site/js/data/sources.js
@@ -5,7 +5,8 @@ export const SPECIES_PATHS = [
   "data/mushrooms/mushrooms-boletes-porcini-tylopilus.json",
   "data/mushrooms/mushrooms-boletes-red-caution.json",
   "data/mushrooms/mushrooms-boletes-suillus-leccinum.json",
-  "data/mushrooms/mushrooms-boletes-other.json"
+  "data/mushrooms/mushrooms-boletes-other1.json",
+  "data/mushrooms/mushrooms-boletes-other2.json"
 ];
 
 export const PHOTO_PATCH_PATHS = [
