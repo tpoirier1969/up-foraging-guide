cefe6c19bc01314c60e17d521713bca3d4c1d4b4
Tod <tpoirier@nmu.edu>
2026-04-30 12:42:27 -0400

    Add files via upload

--- fixed-site/js/data/load-app-data.js
+++ fixed-site/js/data/load-app-data.js
@@ -1,6 +1,6 @@
 import { fetchJsonFromRepo } from "../lib/fetch-json.js";
-import { mergeRecordLayers, normalizeRecord } from "../lib/merge.js?v=v4.2.86-r2026-04-30-photo-patch-fill-only1";
-import { SPECIES_PATHS, PHOTO_PATCH_PATHS, OPTIONAL_PATHS } from "./sources.js?v=v4.2.86-r2026-04-30-photo-patch-fill-only1";
+import { mergeRecordLayers, normalizeRecord } from "../lib/merge.js?v=v4.2.87-r2026-04-30-mushroom-suillus-leccinum-credibility4";
+import { SPECIES_PATHS, PHOTO_PATCH_PATHS, OPTIONAL_PATHS } from "./sources.js?v=v4.2.87-r2026-04-30-mushroom-suillus-leccinum-credibility4";
 
 let rareCachePromise = null;
 let referencesCachePromise = null;
