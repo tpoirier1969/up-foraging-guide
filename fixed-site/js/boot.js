b328158a03aec750ff256c02cfaa3286ddb5cba2
Tod <tpoirier@nmu.edu>
2026-04-30 16:10:47 -0400

    Add files via upload

--- fixed-site/js/boot.js
+++ fixed-site/js/boot.js
@@ -1,7 +1,7 @@
 const pageRoot = document.getElementById("pageRoot");
 const versionBadge = document.getElementById("versionBadge");
-const APP_VERSION = "v4.2.96-r2026-04-30-red-caution-split1";
-const DISPLAY_VERSION = "V4.2.96-r26-04-30";
+const APP_VERSION = "v4.2.97-r2026-04-30-red-caution-review1";
+const DISPLAY_VERSION = "V4.2.97-r26-04-30";
 
 function esc(value) {
   return String(value ?? "")
